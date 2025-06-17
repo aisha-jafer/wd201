454137a390ba9b5829be01218908493f4fe48224
Apple <imacslim2015@MAC-OS-CATALINA.local>
2025-06-17 13:25:02 +0530

    moved http-server folder out of hello-world

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,13 @@
-const hello = () => {
-  console.log("Hello Node.js!");
-};
-
-hello();
+const fs = require("fs");
+fs.writeFile(
+  "sample.txt",
+  "Hello World. Welcome to Node.js File System module.",
+  (err) => {
+    if (err) throw err;
+    console.log("File created!");
+  }
+);
+fs.readFile("sample.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
